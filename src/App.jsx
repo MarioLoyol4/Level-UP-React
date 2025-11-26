6f757e29426f66c479d714c5043d2abdfde7ad37
Melipilla <melipilla@duoc.acad>
2025-11-26 18:35:43 -0300

    ola

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductoList from './components/ProductoList';
 import ProductoFrom from './components/ProductoFrom';
-
+import Login from './components/organisms/Login';
+import Register from './components/organisms/Register';
 
 function App() {
   return (
     <Router>
       <div>
-        <h1>Producto Management System</h1>
+        <h1>Level-Up</h1>
         <Routes>
-          <Route path="/" element={<ProductoList />} />
+
+
+          <Route path="/" element={<Login />} />
+          <Route path="/productos" element={<ProductoList />} />
           <Route path="/add" element={<ProductoFrom />} />
-          <Route path="/edit/:id" element={<ProductoFrom />} />
+
         </Routes>
       </div>
     </Router>
--- src/components/ProductoList.jsx
+++ src/components/ProductoList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import { login } from '../services/AuthService';
 import ProductoService from '../services/ProductoService';
+import { Link } from 'react-router-dom';
 export default function ProductosPage() {
 const [productos, setProductos] = useState([]);
 useEffect(() => {
@@ -12,6 +13,7 @@ ProductoService.getAllProductos().then(res => setProductos(res.data));
 return (
 <div>
 <h1>Productos</h1>
+ <Link to= '/add'>agregar nuevo producto</Link>
 
 <ul>
 {productos.map(b => (
